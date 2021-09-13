93d4a479ff255db44cf3480a86ea410f07ba0865
madhusudhan <dmadhusudhan98@gmail.com>
2021-09-13 13:24:47 +0530

    small details

--- script.js
+++ script.js
@@ -1,56 +1,8 @@
-// // var cars = [11,2,3,4,5,6,7];
-// // for (var b = 0; b<cars.length; b++){
-// // 	console.log(cars[b])
-// // 	// console.log(b)
-// // }
-// // // console.log(cars.length);
-
-// $.ajax({
-// 	url: "https://opentdb.com/api.php?amount=1",
-// 	type: 'GET',
-// 	dataType: "json",
-// 	success: function(data){
-// 		present_question(data);
-// 	},
-// 	error: function(){
-// 		console.log('there is an a error')
-// 	}
-
-// });
-
-// function present_question(data){
-// 	var question = data.results[0].question;
-// 	var answer = data.results[0].incorrect_answers;
-// 	answer.push(data.results[0].correct_answer);
-// 	let shuffled = answer
-//   		.map((value) => ({ value, sort: Math.random() }))
-//   		.sort((a, b) => a.sort - b.sort)
-//   		.map(({ value }) => value)
-// 	$('#question').text(question);
-// 	for (var a = 0; a < shuffled.length; a++){
-// 		var button =  "<input type='radio' id='" + shuffled[a] + "'name = 'QA' >" + '<label for = ' + shuffled[a] + '>' + shuffled[a]
-// 		$("#buttons").append(button);
-// 		var correctans = [data.results[0].correct_answer];
-// 		var c =  $('input[name=QA].checked')
-// 		// console.log('2',c)
-// 		// console.log('1',correctans)
-// 	}
-// 	var v = 0
-// 	while(c > v){
-// 		if (c == correctans){
-// 			console.log('correct')
-// 		}
-// 		v++
-// 	}
-// }
-
 const question = document.getElementById("question");
 const choice = Array.from(document.getElementsByClassName("choice-text"));
 const scores = document.getElementById("scoretext");
-// const loader_ = document.getElementById("loader--");
 const loader = document.getElementById("loader");
 const game = document.getElementById("container");
-// let display_none = document.getElementById("content").style.display = "none";
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -100,7 +52,6 @@ startGame = () => {
   getNewQuestion();
   game.classList.remove("hidden");
   loader.classList.add("hidden");
-  // loader_.classList.add(" hidden");
 };
 
 getNewQuestion = () => {
